0bd29753ffdcddbf31ac2071ac2197d5f8166b8e
Koray Zorluoglu <koray.zorluoglu@esono.de>
2021-12-02 17:28:47 +0100

    ref #style prototype

--- web/setup/Controller/HomeController.php
+++ web/setup/Controller/HomeController.php
@@ -5,9 +5,6 @@ class HomeController extends BaseController implements PageControllerInterface
 
     public function index(): void
     {
-        $this->render('home', [
-            'title' => 'Home',
-            'content' => 'Welcome to the home page.',
-        ]);
+        $this->render('home');
     }
 }
--- web/setup/View/home.php
+++ web/setup/View/home.php
@@ -1,4 +1,28 @@
-<?php echo $title; ?>
-<?php echo $content; ?>
+<?php include "layout/header.php"; ?>
 
-<a href="setup?page=requirement">Go to Requirement</a>
+<div class="card border-0">
+    <div class="card-body p-0">
+        <div class="row no-gutters">
+            <div class="col-lg-6">
+                <div class="p-5">
+                    <div class="mb-5">
+                        <h3 class="h4 font-weight-bold text-theme">Welcome to Chameleon setup wizard</h3>
+                    </div>
+                    <h6 class="h5 mb-0">Follow the steps</h6>
+                    <p class="text-muted mt-2 mb-5">If You Really Want To Know, Look In The Register.</p>
+                </div>
+            </div>
+            <div class="col-lg-6 d-none d-lg-inline-block">
+                <div class="account-block rounded-right">
+                    <div class="overlay rounded-right"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end card-body -->
+</div>
+<!-- end card -->
+<p class="text-muted text-center mt-3 mb-0">Already have an account? <a href="login.html" class="text-primary ml-1">login</a>
+</p>
+
+<?php include "layout/footer.php"; ?>
