0cf91e07748ef28ea9ba21e36022cd2cca1117cf
Koray Zorluoglu <koray.zorluoglu@esono.de>
2021-12-06 09:03:28 +0100

    ref #pdo: create admin with pdo statement

--- web/setup/Controller/CreateadminController.php
+++ web/setup/Controller/CreateadminController.php
@@ -28,7 +28,7 @@ class CreateadminController extends BaseController implements PageControllerInte
                 $this->getPasswordHash($this->request['admin_password'])
             );
 
-            if($user === true) {
+            if ($user === true) {
                 $created = true;
             }
 
@@ -61,36 +61,50 @@ class CreateadminController extends BaseController implements PageControllerInte
      */
     private function createUser(PDO $pdo, string $userId, string $username, string $password, string $email)
     {
-
-        $query = sprintf(
-            "INSERT INTO `cms_user` 
+        $pdo->beginTransaction();
+        $statement = $pdo->prepare(
+            'INSERT INTO `cms_user` 
     (`id`, `cmsident`, `email`, `login`,  `crypted_pw`, `name`, `cms_language_id`, `languages`,
      `images`, `cms_current_edit_language`,
      `allow_cms_login`, `task_show_count`, `is_system`,
      `show_as_rights_template`, `user_tbl_conf_hidden`,
      `cms_workflow_transaction_id`) 
-     VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s',
-             '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')",
-            $userId,
-            2,
-            $email,
-            $username,
-            $password,
-            $username,
-            self::CMS_DEFAULT_LANGUAGE_ID,
-            'en',
-            '1',
-            'en',
-            '1',
-            '5',
-            '1',
-            '1',
-            null,
-            null,
+     VALUES
+            (:id, :cmsident, :email, :login, :crypted_pw, :name, :cms_language_id, :languages,
+             :images, :cms_current_edit_language,
+             :allow_cms_login, :task_show_count, :is_system,
+             :show_as_rights_template, :user_tbl_conf_hidden,
+             :cms_workflow_transaction_id)'
         );
 
+        $defaultIdValue = 1;
+        $randomInt = random_int($defaultIdValue, 999999999);
+        $CMSDEFAULTLANGUAGEID = self::CMS_DEFAULT_LANGUAGE_ID;
+        $defaultLanguage = 'en';
+        $defaultNullValue = null;
+        $taskShowCount = 10;
+
+        $statement->bindParam(':id', $userId, PDO::PARAM_STR);
+        $statement->bindParam(':cmsident', $randomInt, PDO::PARAM_STR);
+        $statement->bindParam(':email', $email, PDO::PARAM_STR);
+        $statement->bindParam(':login', $username, PDO::PARAM_STR);
+        $statement->bindParam(':crypted_pw', $password, PDO::PARAM_STR);
+        $statement->bindParam(':name', $username, PDO::PARAM_STR);
+
+        $statement->bindParam(':cms_language_id', $CMSDEFAULTLANGUAGEID, PDO::PARAM_INT);
+        $statement->bindParam(':languages', $defaultLanguage, PDO::PARAM_STR);
+        $statement->bindParam(':images', $defaultIdValue, PDO::PARAM_INT);
+        $statement->bindParam(':cms_current_edit_language', $defaultLanguage, PDO::PARAM_INT);
+        $statement->bindParam(':allow_cms_login', $defaultIdValue, PDO::PARAM_INT);
+        $statement->bindParam(':task_show_count', $taskShowCount, PDO::PARAM_INT);
+        $statement->bindParam(':is_system', $defaultIdValue, PDO::PARAM_INT);
+        $statement->bindParam(':show_as_rights_template', $defaultIdValue, PDO::PARAM_INT);
+        $statement->bindParam(':user_tbl_conf_hidden', $defaultIdValue, PDO::PARAM_INT);
+        $statement->bindParam(':cms_workflow_transaction_id', $defaultIdValue , PDO::PARAM_INT);
+
         try {
-            $pdo->exec($query);
+            $statement->execute();
+
             return true;
         } catch (\PDOException $e) {
             return $e;
@@ -105,6 +119,7 @@ class CreateadminController extends BaseController implements PageControllerInte
         $uuid .= substr($chars, 12, 4).'-';
         $uuid .= substr($chars, 16, 4).'-';
         $uuid .= substr($chars, 20, 12);
+
         return $uuid;
     }
 }
