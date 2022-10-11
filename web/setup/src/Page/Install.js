41edf8c7cf3a6903394d376e4401304b809e3644
kzorluoglu <koray@d8devs.com>
2022-10-11 19:28:36 +0200

    ref #react: event source with state refactored

--- web/setup/src/Page/Install.js
+++ web/setup/src/Page/Install.js
@@ -1,13 +1,15 @@
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import {LinkContainer} from 'react-router-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import Alert from 'react-bootstrap/Alert';
+
 import {
     BsFillArrowLeftSquareFill,
     BsFillArrowRightSquareFill,
     BsArrowRepeat
 } from "react-icons/bs";
-import {Component} from "react";
+import { Component } from "react";
 
 
 export default class Install extends Component {
@@ -18,6 +20,7 @@ export default class Install extends Component {
         this.state = {
             data: [],
             installable: false,
+            isLoaded: true,
         }
 
         const output = document.getElementById('output');
@@ -25,19 +28,20 @@ export default class Install extends Component {
         let eventSource = new EventSource("http://localhost:3333/setup/index.php?page=composerinstall")
         eventSource.addEventListener('message', e => {
 
-                this.addNewLine(e.data);
-                if (e.data === 'Done!') {
-                    eventSource.close()
-                    this.setState({
-                        installable: true
-                    })
-                    return;
-                }
-
-                if (output) {
-                    output.scrollTop = output.scrollHeight;
-                }
+            this.addNewLine(e.data);
+            if (e.data === 'Done!') {
+                eventSource.close()
+                this.setState({
+                    installable: true,
+                    isLoaded: true
+                })
+                return;
             }
+
+            if (output) {
+                output.scrollTop = output.scrollHeight;
+            }
+        }
         )
         eventSource.addEventListener('close', () =>
             eventSource.close()
@@ -68,51 +72,60 @@ export default class Install extends Component {
 
 
     render() {
-        const {data, installable} = this.state;
-        return <Row>
-            <Col>
-                <div className="p-5">
-                    <div className="mb-5">
-                        <span className="badge bg-secondary">Step 3</span>
-                        <h3 className="h4 font-weight-bold text-theme">Installation</h3>
-                    </div>
-                    <div id="output" className="terminal">
-                        {data.map(item => (
-                            <p key={item}>{item}</p>
-                        ))}
-                    </div>
+        const { isLoaded, data, installable } = this.state;
+        if (isLoaded === false) {
+            return <Row>
+                <Col>
+                    <Alert key="info" variant="info">
+                        Loading..
+                    </Alert>
+                </Col>
+            </Row>
+        } else {
+            return <Row>
+                <Col>
+                    <div className="p-5">
+                        <div className="mb-5">
+                            <span className="badge bg-secondary">Step 3</span>
+                            <h3 className="h4 font-weight-bold text-theme">Installation</h3>
+                        </div>
+                        <div id="output" className="terminal">
+                            {data.map(item => (
+                                <p key={item}>{item}</p>
+                            ))}
+                        </div>
 
-                    {installable ?
-                        <p className="text-success mt-2 mb-5">Congratulations! Everything looks good, you can now
-                            install the software. </p>
-                        :
-                        <p className="text-danger mt-2 mb-5">Ups, the system requirements are not met, please try
-                            again
-                            after completing the required items from the list.</p>
-                    }
+                        {installable ?
+                            <p className="text-success mt-2 mb-5">Congratulations! Everything looks good, you can now
+                                install the software. </p>
+                            :
+                            <p className="mt-2 mb-5">&nbsp;</p>
+                        }
 
-                    <Row>
-                        <Col>
-                            <LinkContainer to="/requirements">
-                                <Button variant="primary"><BsFillArrowLeftSquareFill/> Check System
-                                    requirements</Button>
-                            </LinkContainer>
-                        </Col>
-                        <Col>
-                            {installable ?
-                                <LinkContainer to="/database">
-                                    <Button variant="primary"><BsFillArrowRightSquareFill/> Database
-                                        Validation</Button>
+
+                        <Row>
+                            <Col>
+                                <LinkContainer to="/requirements">
+                                    <Button variant="primary"><BsFillArrowLeftSquareFill /> Check System
+                                        requirements</Button>
                                 </LinkContainer>
-                                :
-                                <Button variant="primary" onClick={this.pageReload}>
-                                    <BsArrowRepeat/> Re-Install
-                                </Button>
-                            }
-                        </Col>
-                    </Row>
-                </div>
-            </Col>
-        </Row>
+                            </Col>
+                            <Col>
+                                {installable ?
+                                    <LinkContainer to="/database">
+                                        <Button variant="primary"><BsFillArrowRightSquareFill /> Database
+                                            Validation</Button>
+                                    </LinkContainer>
+                                    :
+                                    <Button variant="primary" onClick={this.pageReload}>
+                                        <BsArrowRepeat /> Re-Install
+                                    </Button>
+                                }
+                            </Col>
+                        </Row>
+                    </div>
+                </Col>
+            </Row>
+        }
     }
 }
